e9dd2bc2261908504ffb2902a085d43ebc3a6043
isaparedes <isabela.paredes@correo.ucu.edu.uy>
2024-08-12 11:27:45 -0300

    agregamos la divi

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -20,3 +20,12 @@ public class Multiplication
         return a * b;
     }
 }
+
+public class Division
+{
+    public static double Divide(int a, int b)
+    {
+        return (double)a / b;
+    }
+}
+
